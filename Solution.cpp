2f74734a2c1c7e669b3e5212ce2e64cc9631109c
Marcin Putra <puertaputra@gmail.com>
2020-09-02 00:13:01 +0200

    PlusOne 66 is done!

--- Solution.cpp
+++ Solution.cpp
@@ -1078,8 +1078,31 @@ std::string Solution::countAndSay(int n)
 
 
 std::vector<int> Solution::plusOne(std::vector<int>& digits) {
-
 	
+	if (digits[digits.size() - 1] == 9)
+	{
+		int i = digits.size() - 1;
+		while (digits[i] == 9 && i!=0)
+		{
+			digits[i] = 0;
+			i--;
+		}
+		if (digits[i] == 9)
+		{
+			digits[i] = 1;
+			digits.push_back(0);
+		}
+		else
+		{
+			digits[i] += 1;
+		}
+		
+	}
+	else
+	{
+		digits[digits.size() - 1] += 1;
+	}
+	return digits;
 }
 
 
