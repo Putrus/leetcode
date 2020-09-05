2caeca9ecfe954fd5cc7cce3e9216d276442b119
Marcin Putra <puertaputra@gmail.com>
2020-09-06 00:06:03 +0200

    198 is done!

--- Solution.cpp
+++ Solution.cpp
@@ -1173,5 +1173,26 @@ std::vector<int> Solution::plusOne(std::vector<int>& digits) {
 
 int Solution::rob(std::vector<int>& nums)
 {
+	if (nums.size() == 0)
+	{
+		return 0;
+	}
+	if (nums.size() == 1)
+	{
+		return nums[0];
+	}
+	if (nums.size() == 2)
+	{
+		return std::max(nums[0],nums[1]);
+	}
+	std::vector<int> temp(nums.size(), 0);
+
+	temp[0] = nums[0];
+	temp[1] = std::max(nums[0], nums[1]);
+	for (int i = 2; i < nums.size(); i++)
+	{
+		temp[i] = std::max(nums[i] + temp[i - 2], temp[i - 1]);
+	}
+	return temp[nums.size() - 1];
 
 }
