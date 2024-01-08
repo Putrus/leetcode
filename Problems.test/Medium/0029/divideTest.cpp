32b51391512ac3fa2588fda1fb4f7c37f816b709
Marcin Putra <marcin.putra.0@gmail.com>
2024-01-08 20:18:32 +0100

    29. Divide Two Integers

--- Problems.test/Medium/0029/divideTest.cpp
+++ Problems.test/Medium/0029/divideTest.cpp
@@ -24,6 +24,26 @@ namespace Problems
 				{
 					Assert::AreEqual(-2, solution.divide(7, -3));
 				}
+
+				TEST_METHOD(MyTest1)
+				{
+					Assert::AreEqual(0, solution.divide(0, 1));
+				}
+
+				TEST_METHOD(MyTest2)
+				{
+					Assert::AreEqual(std::numeric_limits<int>::max(), solution.divide(std::numeric_limits<int>::min(), -1));
+				}
+
+				TEST_METHOD(MyTest3)
+				{
+					Assert::AreEqual(0, solution.divide(-1020450018, 2091335377));
+				}
+
+				TEST_METHOD(MyTest4)
+				{
+					Assert::AreEqual(1000000000, solution.divide(1000000000, 1));
+				}
 			};
 		}
 	}
--- Problems/Medium/0029/divide.cpp
+++ Problems/Medium/0029/divide.cpp
@@ -1,12 +1,88 @@
 #include "../../Solution/include/Solution.h"
 
 /*
-Runtime: 0 ms, faster than 00.00% of C++ online submissions for Divide Two Integers.
+Runtime: 2395 ms, faster than 5.03% of C++ online submissions for Divide Two Integers.
 
-Memory Usage: 00.00 MB, less than 00.00% of C++ online submissions for Divide Two Integers.
+Memory Usage: 6.36 MB, less than 36.48% of C++ online submissions for Divide Two Integers.
 */
 
 int Solution::divide(int dividend, int divisor)
 {
-   return 0;
+   if (dividend == 0 || divisor == 0)
+   {
+      return 0;
+   }
+
+   if (dividend == std::numeric_limits<int>::min() && divisor == -1)
+   {
+      return std::numeric_limits<int>::max();
+   }
+
+   if (divisor == 1)
+   {
+      return dividend;
+   }
+
+   if (divisor == -1)
+   {
+      return -dividend;
+   }
+
+   int number = dividend;
+   int result = 0;
+
+   if (dividend > 0)
+   {
+      do
+      {
+         if (divisor > 0)
+         {
+            number -= divisor;
+            ++result;
+         }
+         else
+         {
+            number += divisor;
+            --result;
+         }
+      } while (number > 0);
+
+      if (number < 0 && result > 0)
+      {
+         --result;
+      }
+
+      if (number < 0 && result < 0)
+      {
+         ++result;
+      }
+   }
+   else
+   {
+      do
+      {
+         if (divisor > 0)
+         {
+            number += divisor;
+            --result;
+         }
+         else
+         {
+            number -= divisor;
+            ++result;
+         }
+      } while (number < 0);
+
+      if (number > 0 && result > 0)
+      {
+         --result;
+      }
+
+      if (number > 0 && result < 0)
+      {
+         ++result;
+      }
+   }
+  
+   return result;
 }
