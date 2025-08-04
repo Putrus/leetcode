55433275c0c2243805f695409da79f8947f94b2b
Marcin Putra <marcin@putra.pro>
2025-08-04 13:07:45 +0200

    1. Two Sum Cpp
    Runtime 3 ms Beats 70.64%
    Memory 14.87 MB Beats 36.17%
    
    Complexity time O(n), memory O(n)

---
+++ cpp/src/two_sum.cpp
@@ -0,0 +1,41 @@
+#include "../inc/solution.h"
+#include <unordered_map>
+#include <vector>
+
+std::vector<int> Solution::twoSum(std::vector<int>& nums, int target)
+{
+   std::unordered_map<int, int> map;
+   for (int i = 0; i < nums.size(); ++i)
+   {
+      int complement = target - nums[i];
+      if (map.count(complement))
+      {
+         return {map[complement], i};
+      }
+      map[nums[i]] = i;
+   }
+   return {};
+}
+
+#include <gtest/gtest.h>
+
+TEST(TwoSumTest, Example1) {
+   std::vector<int> nums = {2, 7, 11, 15};
+   int target = 9;
+   auto result = Solution().twoSum(nums, target);
+   EXPECT_EQ(result, (std::vector<int>{0, 1}));
+}
+
+TEST(TwoSumTest, Example2) {
+   std::vector<int> nums = {3, 2, 4};
+   int target = 6;
+   auto result = Solution().twoSum(nums, target);
+   EXPECT_EQ(result, (std::vector<int>{1, 2}));
+}
+
+TEST(TwoSumTest, Example3) {
+   std::vector<int> nums = {3, 3};
+   int target = 6;
+   auto result = Solution().twoSum(nums, target);
+   EXPECT_EQ(result, (std::vector<int>{0, 1}));
+}
