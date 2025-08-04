05ad5d94adf341e119c70abd1c8b4328a44f164d
Marcin Putra <marcin@putra.pro>
2025-08-04 13:09:15 +0200

    Change brackets to better style

--- cpp/src/two_sum.cpp
+++ cpp/src/two_sum.cpp
@@ -19,23 +19,26 @@ std::vector<int> Solution::twoSum(std::vector<int>& nums, int target)
 
 #include <gtest/gtest.h>
 
-TEST(TwoSumTest, Example1) {
-   std::vector<int> nums = {2, 7, 11, 15};
+TEST(TwoSumTest, Example1)
+{
+   std::vector<int> nums = { 2, 7, 11, 15 };
    int target = 9;
    auto result = Solution().twoSum(nums, target);
-   EXPECT_EQ(result, (std::vector<int>{0, 1}));
+   EXPECT_EQ(result, (std::vector<int>{ 0, 1 }));
 }
 
-TEST(TwoSumTest, Example2) {
-   std::vector<int> nums = {3, 2, 4};
+TEST(TwoSumTest, Example2)
+{
+   std::vector<int> nums = { 3, 2, 4 };
    int target = 6;
    auto result = Solution().twoSum(nums, target);
-   EXPECT_EQ(result, (std::vector<int>{1, 2}));
+   EXPECT_EQ(result, (std::vector<int>{ 1, 2 }));
 }
 
-TEST(TwoSumTest, Example3) {
-   std::vector<int> nums = {3, 3};
+TEST(TwoSumTest, Example3)
+{
+   std::vector<int> nums = { 3, 3 };
    int target = 6;
    auto result = Solution().twoSum(nums, target);
-   EXPECT_EQ(result, (std::vector<int>{0, 1}));
+   EXPECT_EQ(result, (std::vector<int>{ 0, 1 }));
 }
