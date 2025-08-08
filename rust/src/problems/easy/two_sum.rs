5518c15122dc3295759a2a5f3c726a011c08ada3
Marcin Putra <marcin@putra.pro>
2025-08-08 14:37:49 +0200

    1. Two Sum Rust
    Runtime: 0 ms Beats 100%
    Memory: 2.26 MB Beats 80.55%
    
    Complexity
    Time: O(n)
    Memory: O(n)

--- rust/src/problems/easy/two_sum.rs
+++ rust/src/problems/easy/two_sum.rs
@@ -1,13 +1,16 @@
 use crate::Solution;
+use std::collections::HashMap;
+use std::vec;
 
 impl Solution {
    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-      let mut map = std::collections::HashMap::new();
-      for (i, &num) in nums.iter().enumerate() {
-         if let Some(&j) = map.get(&(target - num)) {
-            return vec![j as i32, i as i32];
+      let mut map: HashMap<i32, i32> = HashMap::new();
+      for (i, num) in nums.iter().enumerate() {
+         let diff = target - num;
+         if map.contains_key(&diff) {
+            return vec![map[&diff], i as i32]
          }
-         map.insert(num, i);
+         map.insert(*num, i as i32);
       }
       vec![]
    }
